bd7912ce467033f0fe15062de81ba11d0f58658b
Anthony Pasquale <apasquale@smsmt.com>
2014-12-08 09:30:03 +1100

    Added chocolatey template and specflow package

---
+++ specflow/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyVsixPackage "SpecFlow" https://visualstudiogallery.msdn.microsoft.com/90ac3587-7466-4155-b591-2cd4cc4401bc/file/112721/3/TechTalk.SpecFlow.Vs2013Integration.vsix 12
